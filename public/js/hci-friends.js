db026e5cd27ad0ca9cc41efdbcb28e525e510bb9
Leta He <leta.he.415@gmail.com>
2015-02-06 23:23:58 +0000

    putting it together lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -3,6 +3,46 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+
+	function anagrammedName(name) {
+	// Thanks, Internet Anagram Server!
+	
+		if (name == "Doug Engelbart") {
+			return "Notable Grudge";
+		} 
+		else if (name == "Ivan Sutherland") {
+			return "Vandal Heist Run";
+		}
+		else if (name == "JCR Licklider") {
+			return "Crick Rid Jell";
+		}
+		else if (name == "Vannevar Bush") {
+			return "Ravens Van Hub";
+		}
+		else if (name == "Alan C. Kay") {
+			return "Canal Yak";
+		}
+		else if (name == "Allen Newell") {
+			return "Ellen All New";
+		}
+		else if (name == "Lucy Suchman") {
+			return "Lunacy Chums";
+		}
+		else if (name == "Grace Hopper") {
+			return "Gear Chopper";
+		}
+		else {
+			console.log(name + " not known for anagramming.");
+			return name;
+		}
+	}
+
+
+	$(".friend-div a h3").click(function(e) {
+		e.preventDefault();
+		var anagram = anagrammedName($(this).text());
+		$(this).text(anagram);
+	})
 })
 
 /*
@@ -12,35 +52,3 @@ function initializePage() {
 	console.log("Javascript connected!");
 }
 
-function anagrammedName(name) {
-	// Thanks, Internet Anagram Server!
-	
-	if (name == "Doug Engelbart") {
-		return "Notable Grudge";
-	} 
-	else if (name == "Ivan Sutherland") {
-		return "Vandal Heist Run";
-	}
-	else if (name == "JCR Licklider") {
-		return "Crick Rid Jell";
-	}
-	else if (name == "Vannevar Bush") {
-		return "Ravens Van Hub";
-	}
-	else if (name == "Alan C. Kay") {
-		return "Canal Yak";
-	}
-	else if (name == "Allen Newell") {
-		return "Ellen All New";
-	}
-	else if (name == "Lucy Suchman") {
-		return "Lunacy Chums";
-	}
-	else if (name == "Grace Hopper") {
-		return "Gear Chopper";
-	}
-	else {
-		console.log(name + " not known for anagramming.");
-		return name;
-	}
-}
